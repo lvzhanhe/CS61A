2b294b7fcc9af7cf1992fe280423dced452637f2
Zhanhe Lyu <zhanhe0827@gmail.com>
2017-11-02 15:48:11 -0400

    init lab03

---
+++ lab03/lab03_extra.py
@@ -0,0 +1,67 @@
+from lab03 import *
+
+# Q4
+def skip_mul(n):
+    """Return the product of n * (n - 2) * (n - 4) * ...
+
+    >>> skip_mul(5) # 5 * 3 * 1
+    15
+    >>> skip_mul(8) # 8 * 6 * 4 * 2
+    384
+    """
+    if n == 2:
+        return 2
+    else:
+        return n * skip_mul(n - 2)
+
+# Q5
+def count_up(n):
+    """Print out all numbers up to and including n in ascending order.
+
+    >>> count_up(5)
+    1
+    2
+    3
+    4
+    5
+    """
+    def counter(i):
+        "*** YOUR CODE HERE ***"
+    counter(1)
+
+# Q6
+def is_prime(n):
+    """Returns True if n is a prime number and False otherwise.
+
+    >>> is_prime(2)
+    True
+    >>> is_prime(16)
+    False
+    >>> is_prime(521)
+    True
+    """
+    "*** YOUR CODE HERE ***"
+
+# Q7
+def interleaved_sum(n, odd_term, even_term):
+    """Compute the sum odd_term(1) + even_term(2) + odd_term(3) + ..., up
+    to n.
+
+    >>> # 1 + 2^2 + 3 + 4^2 + 5
+    ... interleaved_sum(5, lambda x: x, lambda x: x*x)
+    29
+    """
+    "*** YOUR CODE HERE ***"
+
+# Q8
+def ten_pairs(n):
+    """Return the number of ten-pairs within positive integer n.
+
+    >>> ten_pairs(7823952)
+    3
+    >>> ten_pairs(55055)
+    6
+    >>> ten_pairs(9641469)
+    6
+    """
+    "*** YOUR CODE HERE ***"
